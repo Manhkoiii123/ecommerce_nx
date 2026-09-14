fee61ca9a0e1c9263b921af66e905546e012a5c9
manhtranduc <manhtranduc0202@gmail.com>
2026-09-14 22:31:00 +0700

    setup api gateway, main for auth service

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,21 +1,56 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import express from 'express';
-import * as path from 'path';
+import express from "express";
+import * as path from "path";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
+import axios from "axios";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.set("trust proxy", 1);
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api-gateway!' });
+// rate limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: "Too many requests, please try again later." },
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req: any) => req.ip,
 });
 
-const port = process.env.PORT || 3333;
+app.use(limiter);
+
+// health check
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.get("/gateway-health", (req, res) => {
+  res.send({ message: "Welcome to api-gateway!" });
+});
+
+app.use("/", proxy("http://localhost:6001")); // truy cập auth service (/8080/ => sẽ chuyển hướng đến /6001)
+
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on("error", console.error);
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,31 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API" });
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const host = process.env.HOST ?? "localhost";
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+const server = app.listen(port, () => {
+  console.log(`Auth service is running on port at http://${host}:${port}/api`);
+});
+
+server.on("error", (error) => {
+  console.error(error);
+  process.exit(1);
 });
